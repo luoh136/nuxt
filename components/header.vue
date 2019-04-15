fd355ba51b0506e33efd290eeee8a9a56ea4c822
luoh136 <916130659@qq.com>
2019-04-15 09:43:24 +0800

    代码上传

---
+++ components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    {{ msg }}
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      msg: '公共头部内容'
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: block;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  background: #ccc;
+}
+</style>
---
+++ pages/test/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    {{ msg }}
+    <div>
+      <span>
+        {{ test }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world',
+      test: 'this is my first test'
+    }
+  },
+  head: function() {
+    return {
+      title: this.test,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        },
+        {
+          name: 'keywords',
+          content: 'My keywords'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+span {
+  color: red;
+}
+</style>
